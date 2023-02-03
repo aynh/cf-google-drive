73a6bbe69b4ff61a48583312f5ae724c93f8babb
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-03 18:06:17 +0800

    chore: vitest in-source dead code elimination
    https://vitest.dev/guide/in-source.html#production-build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+	define: { 'import.meta.vitest': 'undefined' },
 	envPrefix: process.env.VITEST ? 'TEST_' : 'APP_',
 	plugins: [sveltekit()],
 	test: {
