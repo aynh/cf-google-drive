afa6daf50a310863b536324258f68cb21fce5c54
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-02-07 19:39:00 +0800

    fix: accept multiple spaces after `Basic` authorization header

--- src/lib/server/authentication/basic.ts
+++ src/lib/server/authentication/basic.ts
@@ -1,5 +1,5 @@
 export const parseBasicAuthorizationHeader = (header: string) => {
-	const credentials = header.match(/Basic (.+)/)?.[1];
+	const credentials = header.match(/Basic +(.+)/)?.[1];
 	const split = atob(credentials ?? '').split(':');
 	return { username: split.at(0)!, password: split.at(1) ?? '' };
 };
@@ -12,7 +12,8 @@ if (import.meta.vitest) {
 		{ header: 'invalid', username: '', password: '' },
 		{ header: 'Basic eWFzc2VyOg==', username: 'yasser', password: '' },
 		{ header: 'Basic OmFo', username: '', password: 'ah' },
-		{ header: 'Basic eWFzc2VyOmFo', username: 'yasser', password: 'ah' }
+		{ header: 'Basic eWFzc2VyOmFo', username: 'yasser', password: 'ah' },
+		{ header: 'Basic    eWFzc2VyOmFo', username: 'yasser', password: 'ah' }
 	])('`$header` parsed into `$username:$password`', ({ header, password, username }) => {
 		it('should parse', () => {
 			expect(parseBasicAuthorizationHeader(header)).toStrictEqual({ username, password });
