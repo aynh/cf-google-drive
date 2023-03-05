33aa65921ed5bd48762337abb7ac15f9d25dd360
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-03-05 22:47:01 +0800

    chore: merge vitest config into vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import type { UserConfig } from 'vitest/config';
 
 const config: UserConfig = {
 	define: { 'import.meta.vitest': 'undefined' },
-	envPrefix: 'APP_',
+	envPrefix: process.env.VITEST ? 'TEST_' : 'APP_',
 	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		includeSource: ['src/**/*.{js,ts}'],
+		setupFiles: 'src/lib/server/google-drive-v3/setup-test.ts',
+	},
 };
 
 export default config;
